b1e99b6ff7de95bd3a042c70a2c6143a78dd0ad2
Steven Sequeira <stesequeira11@gmail.com>
2024-11-01 14:50:54 -0600

    Obtener proyectos con api

--- app/userFlow/Proyectos.jsx
+++ app/userFlow/Proyectos.jsx
@@ -1,19 +1,48 @@
 // userFlow/proyectos/Proyectos.jsx
 
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Stack } from "expo-router";
-// import NavContainer from '../../../components/navbarDisplay';
 import NavBarDisplay from '../../components/navbarDisplay';
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function Proyectos() {
+    const [projects, setProjects] = useState([]);
+
+    const handleGetProjects = async () => {
+        const storedUrl = await AsyncStorage.getItem('API_URL');
+        
+        try {
+            // para probar desde el expo go se usa el ipv4 local en las routes
+            // const response = await axios.post('http://192.168.100.29:3001/users/login', {
+            // desde la web se usa localhost en las routes
+
+            const response = await axios.get(`${storedUrl}/proyectos`);
+    
+            if (response.status === 200) {
+                console.log("Proyectos obtenidos: \n", response.data)
+                setProjects(response.data);
+            } else {
+                Alert.alert('Error', 'No se han podido recibir los proyectos');
+            }
+        } catch (error) {
+            Alert.alert('Error', 'Algo ha salido mal');
+            console.error(error);
+        }
+    };
+
     return (
         <View style={styles.container}>
             <Stack.Screen options={{ headerShown: false }} />
             <View style={styles.contentContainer}>
-                <NavBarDisplay/>
+                <NavBarDisplay />
                 <Text style={styles.title}>Proyectos</Text>
                 <Text style={styles.text}>Bienvenido a la pantalla de Proyectos.</Text>
+                <TouchableOpacity style={styles.button} onPress={handleGetProjects}>
+                    <Text style={styles.buttonText}>Prueba pruebita</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -39,5 +68,16 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontFamily: 'SpaceGrotesk',
         color: 'black',
+    },
+    button: {
+        marginTop: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#007bff',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 16,
     },
 });
