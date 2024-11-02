4b7045294c452b0eae6693fb11570bfbb45e367c
Brayton16 <Brayton2011@hotmail.es>
2024-11-02 02:16:26 -0600

    proyectos y perfil

--- app/adminFlow/Proyectos.jsx
+++ app/adminFlow/Proyectos.jsx
@@ -1,19 +1,109 @@
 // userFlow/proyectos/Proyectos.jsx
 
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
 import { Stack } from "expo-router";
-// import NavContainer from '../../../components/navbarDisplay';
 import NavBarDisplay from '../../components/navbarDisplay';
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+function ProjectCard({ project }) {
+    return(
+        <View style={styles.cardContainer}>
+            <View style={styles.headerContainer}>
+                <View style={styles.avatar}>
+                    <Text style={styles.avatarText}>{project.ownerInitials}</Text>
+                </View>
+                <View style={styles.headerTextContainer}>
+                    <Text style={styles.headerTitle}>{project.FirstName}</Text>
+                    <Text style={styles.headerSubtitle}>{project.WorkArea.toUpperCase()}</Text>
+                </View>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={{ uri: project.imageUri }}
+                    resizeMode="cover"
+                />
+            </View>
+            <View style={styles.contentContainer}>
+                <Text style={styles.projectTitle}>{project.ProjectName}</Text>
+                <Text style={styles.projectSubtitle}>{project.Category}</Text>
+                <Text style={styles.projectDescription}>{project.ProjectDescription}</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Donaciones</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Conocer</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
 
 export default function Proyectos() {
+    const [projects, setProjects] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    useEffect(() => {
+        const handleGetProjects = async () => {
+            const storedUrl = await AsyncStorage.getItem('API_URL');
+            try {
+                const response = await axios.get(`${storedUrl}/proyectos`);
+                if (response.status === 200) {
+                    setProjects(response.data);
+                } else {
+                    Alert.alert('Error', 'No se han podido recibir los proyectos');
+                }
+            } catch (error) {
+                Alert.alert('Error', 'Algo ha salido mal');
+                console.error(error);
+            }
+        };
+
+        handleGetProjects();
+    }, []);
+
     return (
         <View style={styles.container}>
             <Stack.Screen options={{ headerShown: false }} />
             <View style={styles.contentContainer}>
                 <NavBarDisplay/>
-                <Text style={styles.title}>Proyectos</Text>
-                <Text style={styles.text}>Bienvenido a la pantalla de Proyectos.</Text>
+                <ScrollView style={{marginTop: 80, marginHorizontal: 30}} showsVerticalScrollIndicator={false}>
+                <View style={styles.filterContainer}>
+                        <TouchableOpacity style={styles.filterButton}>
+                            <Text style={styles.filterButtonText}>Categoría</Text>
+                        </TouchableOpacity>                    
+                        <TouchableOpacity style={styles.filterButton}>
+                            <Text style={styles.filterButtonText}>Recaudado</Text>
+                        </TouchableOpacity>                    
+                        <TouchableOpacity style={styles.filterButton}>
+                            <Text style={styles.filterButtonText}>Meta</Text>
+                        </TouchableOpacity>                    
+                        <TouchableOpacity style={styles.filterButton}>
+                            <Text style={styles.filterButtonText}>Fecha límite</Text>
+                        </TouchableOpacity>                    
+                    </View>
+                    <View style={styles.SearchBar}>
+                        <Ionicons name="search" size={20} color="black" style={{marginRight: 10, marginLeft: 2}} />
+                        <TextInput
+                            style={{fontFamily: 'SpaceGrotesk', paddingHorizontal: 5, flex: 1}}
+                            placeholder="Buscar proyectos"
+                            placeholderTextColor={'black'}
+                            value={searchQuery}
+                            onChangeText={setSearchQuery}
+                            
+                        />
+                    </View>
+                    {projects ? projects.map((project) => (
+                        <ProjectCard key={project.ProjectID} project={project} />
+                    )) : <Text style={styles.text}>Cargando proyectos...</Text>}
+                </ScrollView>
             </View>
         </View>
     );
@@ -40,4 +130,130 @@ const styles = StyleSheet.create({
         fontFamily: 'SpaceGrotesk',
         color: 'black',
     },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontFamily: 'SpaceGrotesk',
+    },
+    cardContainer: {
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
+        padding: 15,
+        marginVertical: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3,
+    },
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    avatar: {
+        backgroundColor: '#6A1B9A',
+        borderRadius: 25,
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    avatarText: {
+        color: '#ffffff',
+        fontSize: 18,
+        fontFamily: 'SpaceGrotesk',
+    },
+    headerTextContainer: {
+        marginLeft: 10,
+    },
+    headerTitle: {
+        fontSize: 16,
+        fontFamily: 'SpaceGrotesk',
+    },
+    headerSubtitle: {
+        fontSize: 12,
+        color: '#666',
+    },
+    imageContainer: {
+        backgroundColor: '#F8BBD0', // Placeholder color
+        height: 150,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
+    },
+    contentContainer: {
+        marginBottom: 10,
+    },
+    projectTitle: {
+        fontSize: 18,
+        fontFamily: 'SpaceGrotesk',
+        marginBottom: 5,
+    },
+    projectSubtitle: {
+        fontSize: 14,
+        color: '#666',
+        marginBottom: 5,
+        fontFamily: 'SpaceGrotesk',
+    },
+    projectDescription: {
+        fontSize: 14,
+        color: '#333',
+        fontFamily: 'SpaceGrotesk',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginTop: 10,
+    },
+    button: {
+        backgroundColor: '#04233B',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 50,
+    },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 14,
+        fontFamily: 'SpaceGrotesk',
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 10,
+        marginTop: 10,
+    },
+    filterButton: {
+        backgroundColor: '#1C7690',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderRadius: 50,
+    },
+    filterButtonText: {
+        color: '#ffffff',
+        fontSize: 14,
+        fontFamily: 'SpaceGrotesk',
+    },
+    filterButtonActive: {
+        backgroundColor: '#EF5356',
+    },
+    SearchBar: {
+        backgroundColor: '#F3EDF7',
+        borderRadius: 50,
+        padding: 10,
+        marginBottom: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3,
+        flexDirection: 'row',
+        alignItems: 'center',
+    }
 });
+
--- app/userFlow/Perfil.jsx
+++ app/userFlow/Perfil.jsx
@@ -1,38 +1,159 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { Stack } from "expo-router";
 import NavBarDisplay from '../../components/navbarDisplay';
-export default function Perfil() {
+import { useEffect, useState } from 'react';
+
+
+export default function Profile() {
+
+    const [userData, setUserData] = useState(null);
+    // Simulación de datos de usuario
+    const simData = {
+        UserID: 9,
+        FirstName: "Brayton",
+        LastName: "Solano",
+        Cedula: "118640057",
+        Email: "bsolano@estudiantec.cr",
+        WorkArea: "computación",
+        DigitalMoney: 500,
+        PhoneNumber: "62668621",
+        IsActive: true,
+        CreatedAt: "2024-09-17T22:35:59.417Z",
+        Rol: 'Usuario',
+        ProfilePhoto: 'https://scontent.fsjo10-1.fna.fbcdn.net/v/t39.30808-1/447462029_7509873185757609_8697984359719734285_n.jpg?stp=dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=nfAnNnqcq2kQ7kNvgHJRClu&_nc_zt=24&_nc_ht=scontent.fsjo10-1.fna&_nc_gid=AW1gDXCqQuhoYZWQBUFzfYU&oh=00_AYDGDgTp6qbD8zQBJDTBwOFOEXkZfa0rVn0YZE9-nmMQuQ&oe=672B9487'
+    }
+
     return (
-        <View style={styles.container}>
-            <Stack.Screen options={{ headerShown: false }} />
-            <View style={styles.contentContainer}>
-                <NavBarDisplay/>
-                <Text style={styles.title}>Perfil</Text>
-                <Text style={styles.text}>Bienvenido a la pantalla de Perfil.</Text>
+        <ScrollView contentContainerStyle={styles.container}>
+            <NavBarDisplay/>
+            <View style={styles.profileContainer}>
+                <Image
+                    style={styles.profileImage}
+                    source={{ uri: simData.ProfilePhoto }}
+                />
+                <Text style={styles.profileName}>{simData.FirstName} {simData.LastName}</Text>
+                <View style={styles.moneyContainer}>
+                    <Text style={styles.moneyText}>Saldo: {simData.DigitalMoney} </Text>
+                    <Image style={styles.moneyImage} source={require('../../assets/goofycoin.png')}/>
+                </View>
             </View>
-        </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoLabel}>Correo</Text>
+                <View style={styles.infoGroup}>
+                    <Text style={styles.infoText}>{simData.Email}</Text>
+                    <TouchableOpacity><Text style={styles.editLink}>Cambiar correo</Text></TouchableOpacity>
+                </View>
+                <Text style={styles.infoLabel}>Contraseña</Text>
+                <View style={styles.infoGroup}>
+                    <Text style={styles.infoText}>**************</Text>
+                    <TouchableOpacity><Text style={styles.editLink}>Cambiar contraseña</Text></TouchableOpacity>
+                </View>
+                <Text style={styles.infoLabel}>Cédula</Text>
+                <Text style={styles.infoText}>{simData.Cedula}</Text>
+                <Text style={styles.infoLabel}>Teléfono</Text>
+                <View style={styles.infoGroup}>                    
+                    <Text style={styles.infoText}>{simData.PhoneNumber}</Text>
+                    <TouchableOpacity><Text style={styles.editLink}>Cambiar teléfono</Text></TouchableOpacity>
+                </View>
+                <Text style={styles.infoLabel}>Rol</Text>
+                <Text style={styles.infoText}>{simData.Rol}</Text>
+                <Text style={styles.infoLabel}>Área de conocimiento</Text>
+                <View style={styles.infoGroup}>
+                    <Text style={styles.infoText}>{simData.WorkArea}</Text>
+                    <TouchableOpacity><Text style={styles.editLink}>Cambiar área</Text></TouchableOpacity>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Recargar GoofyCoins</Text>
+            </TouchableOpacity>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-    },
-    contentContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexGrow: 1,
         backgroundColor: '#ffffff',
+        padding: 20,
+    },
+    menuButton: {
+        marginRight: 10,
+    },
+    menuIcon: {
+        fontSize: 24,
+        fontFamily: 'SpaceGrotesk'
     },
     title: {
-        fontSize: 24,
+        fontSize: 20,
         fontFamily: 'SpaceGrotesk',
+    },
+    profileContainer: {
+        alignItems: 'center',
         marginBottom: 20,
-        color: '#04233B',
+        marginTop: 70,
     },
-    text: {
-        fontSize: 16,
+    profileImage: {
+        width: 80,
+        height: 80,
+        borderRadius: 50,
+        marginBottom: 10,
+    },
+    profileName: {
+        fontSize: 22,
+        fontFamily: 'SpaceGrotesk'
+    },
+    infoContainer: {
+        marginBottom: 20,
+    },
+    infoLabel: {
+        fontSize: 18,
         fontFamily: 'SpaceGrotesk',
-        color: 'black',
+        marginTop: 10,
     },
+    infoText: {
+        fontSize: 14,
+        fontFamily: 'SpaceGrotesk',
+        marginBottom: 5,
+    },
+    infoGroup: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    editLink: {
+        color: '#1E90FF',
+        fontSize: 14,
+        fontFamily: 'SpaceGrotesk',
+        marginBottom: 15,
+    },
+    button: {
+        backgroundColor: '#F75657',
+        paddingVertical: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    moneyContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 30,
+        backgroundColor: '#04233B',
+        paddingVertical: 10,
+        paddingHorizontal: 50,
+        borderRadius: 50,
+    },
+    moneyText: {
+        color: '#ffffff',
+        fontSize: 18,
+        fontFamily: 'SpaceGrotesk',
+        marginLeft: 10,
+    },
+    moneyImage: {
+        marginLeft: 10,
+        width: 50,
+        height: 50,
+    }
 });
