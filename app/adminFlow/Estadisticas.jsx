22fde52112a4ecc21e22131c4992a265eeff5fcb
SrJulius <jumadrigal@estudiantec.cr>
2024-11-05 20:06:36 -0600

    Estadísticas

--- app/adminFlow/Estadisticas.jsx
+++ app/adminFlow/Estadisticas.jsx
@@ -1,14 +1,65 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { Stack } from "expo-router";
+import axios from 'axios';
 import NavBarDisplay from '../../components/navbarDisplay';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export default function Estadisticas() {
+    const [estadisticas, setEstadisticas] = useState({
+        usuarios: { total: 0, ultimoMes: 0 },
+        donaciones: { total: 0, ultimoMes: 0 },
+        proyectos: { total: 0, ultimoMes: 0 },
+        publicaciones: { total: 0, ultimoMes: 0 }
+    });
+
+    useEffect(() => {
+        const obtenerEstadisticas = async () => {
+            const storedUrl = await AsyncStorage.getItem('API_URL');
+            try {
+                const response = await axios.get(`${storedUrl}/estadisticas`);
+                if (response.status === 200) {
+                    setEstadisticas(response.data);
+                } else {
+                    Alert.alert('Error', 'No se han podido recibir las estadísticas');
+                }
+            } catch (error) {
+                Alert.alert('Error', 'Algo ha salido mal');
+                console.error(error);
+            }
+        };
+        obtenerEstadisticas();
+    }, []);
+
     return (
         <View style={styles.container}>
             <Stack.Screen options={{ headerShown: false }} />
             <View style={styles.contentContainer}>
-                <NavBarDisplay/>
-                <Text style={styles.title}>Estadísticas</Text>
-                <Text style={styles.text}>Bienvenido a la pantalla de Estadísticas.</Text>
+                <NavBarDisplay />
+                <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+                    <View style={styles.statsContainer}>
+                        <View style={styles.statsBox}>
+                            <Text style={styles.statsTitle}>Usuarios</Text>
+                            <Text style={styles.statsNumber}>{estadisticas.usuarios.total}</Text>
+                            <Text style={styles.statsSubtitle}>Último mes: {estadisticas.usuarios.ultimoMes}</Text>
+                        </View>
+                        <View style={styles.statsBox}>
+                            <Text style={styles.statsTitle}>Donaciones</Text>
+                            <Text style={styles.statsNumber}>{estadisticas.donaciones.total}</Text>
+                            <Text style={styles.statsSubtitle}>Último mes: {estadisticas.donaciones.ultimoMes}</Text>
+                        </View>
+                        <View style={styles.statsBox}>
+                            <Text style={styles.statsTitle}>Proyectos</Text>
+                            <Text style={styles.statsNumber}>{estadisticas.proyectos.total}</Text>
+                            <Text style={styles.statsSubtitle}>Último mes: {estadisticas.proyectos.ultimoMes}</Text>
+                        </View>
+                        <View style={styles.statsBox}>
+                            <Text style={styles.statsTitle}>Publicaciones</Text>
+                            <Text style={styles.statsNumber}>{estadisticas.publicaciones.total}</Text>
+                            <Text style={styles.statsSubtitle}>Último mes: {estadisticas.publicaciones.ultimoMes}</Text>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         </View>
     );
@@ -24,15 +75,45 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#ffffff',
     },
-    title: {
-        fontSize: 24,
+    scrollView: {
+        marginTop: 80,
+        marginHorizontal: 30
+    },
+    statsContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        marginBottom: 20
+    },
+    statsBox: {
+        width: '45%',
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
+        padding: 15,
+        marginVertical: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3,
+    },
+    statsTitle: {
+        fontSize: 18,
         fontFamily: 'SpaceGrotesk',
-        marginBottom: 20,
+        marginBottom: 5,
         color: '#04233B',
     },
-    text: {
-        fontSize: 16,
+    statsNumber: {
+        fontSize: 24,
         fontFamily: 'SpaceGrotesk',
-        color: 'black',
+        marginBottom: 5,
+        color: '#04233B',
     },
+    statsSubtitle: {
+        fontSize: 14,
+        fontFamily: 'SpaceGrotesk',
+        color: '#666',
+    }
 });
+
+export default Estadisticas;
